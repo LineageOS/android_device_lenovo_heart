68002f2140d4b104a5480e6ef092e5e95a93c464
Michael Bestas <mkbestas@lineageos.org>
2024-12-02 01:29:18 +0200

    heart: Convert rootdir to init, Android.mk to bp
    Change-Id: I43ccc55b98231d984474e6bba3c9fa1266bb7351

--- device.mk
+++ device.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2019-2024 The LineageOS Project
+# Copyright (C) 2019-2025 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -31,7 +31,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths_tavil.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_tavil.xml \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml
 
-# Device init scripts
+# Init
 PRODUCT_PACKAGES += \
     init.device.rc \
     init.system.qcom.rc
